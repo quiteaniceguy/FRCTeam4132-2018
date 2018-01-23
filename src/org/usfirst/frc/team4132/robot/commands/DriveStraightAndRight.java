c7323ce010f34f000e84bb062ee7cad7166baea7
thomas eblen <tommyeblen@gmail.com>
2018-01-22 16:10:57 -0800

    changed auto drive speed

--- src/org/usfirst/frc/team4132/robot/commands/DriveStraightAndRight.java
+++ src/org/usfirst/frc/team4132/robot/commands/DriveStraightAndRight.java
@@ -5,10 +5,10 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class DriveStraightAndRight extends CommandGroup{
 	public DriveStraightAndRight() {
 		//drive straight
-		addSequential(new AutoDriveRobot(0, 0, .5, .5, 3));
+		addSequential(new AutoDriveRobot(0, 0, .8, .8, 3));
 		//turn
-		addSequential(new AutoDriveRobot(0, 0, -.5, -.5, 3));
+		addSequential(new AutoDriveRobot(0, 0, -.8, -.8, 3));
 		//dirve straight
-		addSequential(new AutoDriveRobot(.5, .5, -.5, .5, 3));
+		addSequential(new AutoDriveRobot(.8, .8, -.8, .8, 3));
 	}
 }
